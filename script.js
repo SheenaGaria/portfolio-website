2b268bba6b028e139fae708a746fc724e31d82e2
SheenaGaria <sheena.garia@gmail.com>
2025-04-14 14:33:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const projectsSection = document.getElementById("projects");
+  projectsSection.addEventListener("click", () => {
+    alert("You clicked on the Projects section!");
+  });
+});
